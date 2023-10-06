a89e848efe940f846e85994026cb142c2ee60fb9
SH-ur <sdjsm2003@gmail.com>
2023-10-06 10:32:36 -0400

    Modificando ruta local por deploy back

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan('dev'));
 //server.use(cors());
 server.use((req, res, next) => {
   //lo cambié de 3000 a 3001 para que coincida
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://videogame-app-api.onrender.com/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
